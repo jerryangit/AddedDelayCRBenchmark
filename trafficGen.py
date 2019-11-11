b36f47784dc4bbb3bfc7f270012c3364a53a3fb6
jerry <jerryan.x@gmail.com>
2019-11-11 15:30:16 -0500

    Add data output for trafficGen.py

--- trafficGen.py
+++ trafficGen.py
@@ -8,7 +8,8 @@
 import glob
 import os
 import sys
-
+import csv
+import datetime
 try:
     sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
         sys.version_info.major,
@@ -21,18 +22,29 @@ import carla
 import random
 import time
 random.seed(27)
+## Create data folder
 
+if not os.path.exists('./data'):
+    os.makedirs('./data')
 
 def main():
     # CONFIG
     maxVehicle = 30
-    totalVehicle = 100
-    actor_list = []
-    # Initialize values
-    i = 1
-    spwnRand = random.randint(1,4)
-    destRand = random.randint(1,4)
+    totalVehicle = 10
     
+    # Initialize values
+    i = 0
+
+    # spwnRand = random.randint(1,4)
+    # destRand = random.randint(1,4)
+    # Change randint if not 4 way cross road
+    spwnRand = [random.randint(1, 4) for iter in range(totalVehicle)] 
+    destRand = [random.randint(1, 4) for iter in range(totalVehicle)]
+    spwnTime = []
+    destTime = []
+    actor_list = []
+
+
     try:
         # Init carla at port 2000
         client = carla.Client('localhost', 2000)
@@ -40,7 +52,9 @@ def main():
 
         # Retrieve world
         world = client.get_world()
-
+        tick0 = world.wait_for_tick()
+        ts0 = tick0.timestamp
+        ts0s = tick0.timestamp.elapsed_seconds
         # Retrive blueprints
         blueprint_library = world.get_blueprint_library()
 
@@ -68,7 +82,9 @@ def main():
 
         
 
-        while i <= totalVehicle:
+        while i < totalVehicle:
+            tick = world.wait_for_tick()
+            ts = tick.timestamp
             if False:
                 bp = random.choice(blueprint_library.filter('vehicle'))
             else:
@@ -76,27 +92,46 @@ def main():
 
             # occupied by another object, the function will return None.
             
-            spwn = world.try_spawn_actor(bp, spwnLoc[spwnRand])
+            spwn = world.try_spawn_actor(bp, spwnLoc[spwnRand[i]])
 
             if spwn is not None:
                 actor_list.append(spwn)
                 spwn.set_autopilot()
-                print('created %s at %d' % (spwn.type_id,spwnRand))
+                spwnTime.append(ts.elapsed_seconds-ts0s)
+                print('[%d] created %s at %d with id %d' % (i,spwn.type_id,spwnRand[i],spwn.id))
                 i += 1
-                spwnRand = random.randint(1,4)
-                destRand = random.randint(1,4)
-
             for actor in actor_list:
                 
                 if actor.get_location().distance(carla.Location(x=-150, y=-35, z=0.3)) > 38 and actor.get_location().distance(carla.Location(x=0, y=0, z=0)) > 5:
-                    print(actor.id,' left the area at ', actor.get_location().x,actor.get_location().y,actor.get_location().z)
+                    print(actor.id,' left the area at ', round(actor.get_location().x,2),round(actor.get_location().y,2),round(actor.get_location().z,2))
+                    destTime.append(ts.elapsed_seconds-ts0s)
                     actor_list.remove(actor)
-                    actor.destroy()
+                    actor.destroy()             
+        while  len(actor_list) > 0:
+            for actor in actor_list:
+                if actor.get_location().distance(carla.Location(x=-150, y=-35, z=0.3)) > 38 and actor.get_location().distance(carla.Location(x=0, y=0, z=0)) > 5:
+                    print(actor.id,' left the area at ', round(actor.get_location().x,2),round(actor.get_location().y,2),round(actor.get_location().z,2))
+                    destTime.append(ts.elapsed_seconds-ts0s)
+                    actor_list.remove(actor)
+                    actor.destroy() 
+        
+        # print('spwnRand:',spwnRand)
+        # print('destRand:',destRand)
+        # print('spwnTime:',spwnTime)
+        # print('destTime:',destTime)
 
-        time.sleep(60)
 
     finally:
-
+        # Save lists as csv
+        data = []
+        data = zip(spwnRand,destRand,spwnTime,destTime)
+        filename = datetime.datetime.now().strftime('data-%Y-%m-%d-%H-%M.csv')
+        with open('./data/'+filename, 'wb') as log:
+            wr = csv.writer(log, quoting=csv.QUOTE_ALL)
+            for row in data:
+                wr.writerow(row)
+        
+        # Clean up actors
         print('destroying actors')
         for actor in actor_list:
             actor.destroy()
